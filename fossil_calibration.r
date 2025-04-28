172f23e8a01fb8ef0afaf878726df485bbf65188
Avehi <avehi.singh@gmail.com>
2025-04-28 21:23:23 +0530

    Add files via upload

---
+++ fossil_calibration.r
@@ -0,0 +1,28 @@
+library(phangorn)
+library(ape)
+library(phytools)
+
+tree <- read.newick("~/Work/species_phylogeny_eucera/Whole_phylogeny.rooted.newick")
+#calibrate <- makeChronosCalib(tree, node = "root", age.max = 179)
+
+nodes <- c(
+  getMRCA(tree, tip = c("Colletes_gigas", "Osmia_lignaria")),
+  getMRCA(tree, tip = c("Bombus_impatiens", "Melipona_quadrifasciata")),
+  getMRCA(tree, tip = c("Apis_melliFfera", "Euglossa_dilemma")),
+  getMRCA(tree, tip = c("Bombus_impatiens", "Habropoda_laboriosa")),
+  getMRCA(tree, tip = c("Megachile_rotundata", "Osmia_lignaria")),
+  getMRCA(tree, tip = c("Megachile_rotundata", "Melissodes_desponsus"))
+)
+
+age.min <- c(0, 53, 28, 60, 58, 104)
+age.max <- c(179, 179, 179, 179, 179, 179)
+soft.bounds <- c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
+
+mycalibration <- data.frame(nodes, age.min, age.max, soft.bounds)
+
+timetree <- chronos(tree, lambda = 1, model = "relaxed", calibration = mycalibration, control = chronos.control() )
+#timetree <- chronos(tree, lambda = 1, model = "correlated", calibration = mycalibration, control = chronos.control() )
+#timetree <- chronos(tree, lambda = 1, model = "discrete", calibration = mycalibration, control = chronos.control() )
+
+is.ultrametric(timetree)
+write.tree(timetree, "~/Work/species_phylogeny_eucera/Whole_phylogeny.rooted.utrametric.newick")
