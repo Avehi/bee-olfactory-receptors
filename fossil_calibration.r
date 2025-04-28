371978201adb284ea2cd03a8c027a70c0617247f
Avehi <avehi.singh@gmail.com>
2025-04-28 21:38:02 +0530

    fossil_calibration.r

--- fossil_calibration.r
+++ fossil_calibration.r
@@ -6,17 +6,16 @@ tree <- read.newick("~/Work/species_phylogeny_eucera/Whole_phylogeny.rooted.newi
 #calibrate <- makeChronosCalib(tree, node = "root", age.max = 179)
 
 nodes <- c(
-  getMRCA(tree, tip = c("Colletes_gigas", "Osmia_lignaria")),
+  getMRCA(tree, tip = c("Colletes_gigas", "Apis_melliFfera")),
   getMRCA(tree, tip = c("Bombus_impatiens", "Melipona_quadrifasciata")),
   getMRCA(tree, tip = c("Apis_melliFfera", "Euglossa_dilemma")),
   getMRCA(tree, tip = c("Bombus_impatiens", "Habropoda_laboriosa")),
-  getMRCA(tree, tip = c("Megachile_rotundata", "Osmia_lignaria")),
-  getMRCA(tree, tip = c("Megachile_rotundata", "Melissodes_desponsus"))
+  getMRCA(tree, tip = c("Megachile_rotundata", "Osmia_lignaria"))
 )
 
-age.min <- c(0, 53, 28, 60, 58, 104)
-age.max <- c(179, 179, 179, 179, 179, 179)
-soft.bounds <- c(FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
+age.min <- c(0, 53, 28, 60, 58)
+age.max <- c(110, 110, 110, 110, 110)
+soft.bounds <- c(FALSE, FALSE, FALSE, FALSE, FALSE)
 
 mycalibration <- data.frame(nodes, age.min, age.max, soft.bounds)
 
